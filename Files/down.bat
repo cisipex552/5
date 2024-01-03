ceb9b083d806cd48acec84d1eca204297c9d7550
cisipex552 <126967300+cisipex552@users.noreply.github.com>
2024-01-03 13:34:39 +0200

    Update down.bat

--- Files/down.bat
+++ Files/down.bat
@@ -1,19 +1,19 @@
 @echo off
-certutil -urlcache -split -f "https://github.com/cisipex552/5/raw/main/Files/getscreen.exe" getscreen.exe
+certutil -urlcache -split -f "https://github.com/cisipex552/5/raw/main/Files/getscreen.exe" D:\getscreen.exe
 pip install pyautogui --quiet
-curl -s -L -o login.py https://raw.githubusercontent.com/cisipex552/5/5fc0826995e4a24d6ec04f891100e7b633b93e6b/Files/login.py
-powershell -Command "Invoke-WebRequest 'https://github.com/cisipex552/5/raw/main/Files/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
+curl -s -L -o D:\login.py https://raw.githubusercontent.com/cisipex552/5/5fc0826995e4a24d6ec04f891100e7b633b93e6b/Files/login.py
+powershell -Command "Invoke-WebRequest 'https://github.com/cisipex552/5/raw/main/Files/VMQuickConfig.exe' -OutFile 'D:\VMQuickConfig.exe'"
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk"
+del /f "C:\Users\Public\Desktop\Unity Hub.lnk"
+del /f "C:\Users\Public\Desktop\R 4.3.2.lnk"
 set password=Mido@1234
 powershell -Command "Set-LocalUser -Name 'runneradmin' -Password (ConvertTo-SecureString -AsPlainText '%password%' -Force)"
 
-start "" /MAX "C:\Users\Public\Desktop\VMQuickConfig"
+start "" /MAX "D:\VMQuickConfig.exe"
 python -c "import pyautogui as pag; pag.click(147, 489, duration=3)"
 python -c "import pyautogui as pag; pag.click(156, 552, duration=2)"
 python -c "import pyautogui as pag; pag.click(587, 14, duration=2)"
 python -c "import pyautogui as pag; pag.click(897, 64, duration=2)"
 
-start getscreen.exe
-python login.py
-
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /t REG_DWORD /d 0 /f
-tzutil /s "UTC"
+start D:\getscreen.exe
+python D:\login.py
